999c8f0f9010eb74fcbc70b7e13743f255d2c870
Kieran <kieranor96@gmail.com>
2019-04-03 16:18:55 +0100

    groupproject updated

---
+++ groupgame/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public Sprite ActiveSprite;
+    SpriteRenderer sprite;
+
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        //if hit by the player, change the sprite
+        if(collision.gameObject.tag == "Player")
+        {
+            sprite.sprite = ActiveSprite;
+        }
+    }
+}
---
+++ groupgame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : CharacterMovement
+{
+    public float horizontal, vertical;
+    Vector2 customVelocity;
+    PlayerData data;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        data = GetComponent<PlayerData>();
+    }
+
+    void Update()
+    {
+        //get input
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        //setup customVelocity
+        customVelocity.x = horizontal * movementSpeed;
+        customVelocity.y = body.velocity.y;
+
+        //apply customVelocity
+        body.velocity = customVelocity;
+
+        //check for input jump, dash
+        if(Input.GetKeyDown(KeyCode.Space) && isOnJumpingSurface)
+        {
+            Jump();//player is jumping
+            data.State = PlayerState.Jumping;
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Dash(horizontal);//player is dashing
+            data.State = PlayerState.Crouch;
+        }
+
+        if(horizontal != 0)
+        {
+            data.State = PlayerState.Walking;
+        }
+
+        if(!Input.anyKey)//if no key was pressed, player is idle
+        {
+            //idle state
+            data.State = PlayerState.Idle;
+        }
+    }
+}
