11fc244a8a78f91e7a1e6c6417d4aa73cf7da9ce
kieranoreilly96 <kieranor96@gmail.com>
2019-04-04 21:26:58 +0100

    rediting_files

---
+++ groupgame/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject PlayerToFollow;
+    private Vector3 tempPosition;
+    
+
+    void Update()
+    {
+        tempPosition.x = PlayerToFollow.transform.position.x;
+        tempPosition.y = PlayerToFollow.transform.position.y;
+        tempPosition.z = transform.position.z;
+
+        transform.position = tempPosition;
+    }
+}
---
+++ groupgame/Assets/Scripts/MenuController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void LoadScene(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+	
+}
