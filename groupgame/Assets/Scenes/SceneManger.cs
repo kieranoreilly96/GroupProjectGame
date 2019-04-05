54b080e784755a0f4cab8b130368dc5e21c469db
Kieran <kieranor96@gmail.com>
2019-04-05 13:47:46 +0100

    pause started

---
+++ groupgame/Assets/Scenes/SceneManger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    public GameObject pauseScreen;
+
+    public void Pause()
+    {
+        pauseScreen.SetActive(true);
+        Time.timeScale = 0.0f;
+    }
+
+    public void UnPause()
+    {
+        pauseScreen.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            Pause();
+        }
+    }
+}
