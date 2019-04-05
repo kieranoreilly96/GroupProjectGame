3f8df0119053e5946ae252c41a23fcb83ceaa58b
Kieran <kieranor96@gmail.com>
2019-04-05 14:37:19 +0100

    refresh

---
+++ groupgame/Assets/Scripts/PlayerAnimations.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour
+{
+    PlayerData data;
+    PlayerMovement movement;
+    Animator animator;
+    SpriteRenderer sprite;
+
+    void Start()
+    {
+        data = GetComponent<PlayerData>();
+        movement = GetComponent<PlayerMovement>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        //only update animation when the player state has changed
+        if(data.State != data.previousState)
+        {
+            animator.SetInteger("State", (int)data.State);
+            data.previousState = data.State;
+        }
+
+        if(movement.horizontal > 0)//right
+        {
+            sprite.flipX = false;
+        }
+        else if(movement.horizontal < 0)//left
+        {
+            sprite.flipX = true;
+        }
+    }
+}
---
+++ groupgame/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState
+{
+    Idle,//0
+    Walking,//1
+    Jumping,//2
+    Crouch//3
+}
+
+public class PlayerData : MonoBehaviour
+{
+    public PlayerState State = PlayerState.Idle;
+    public PlayerState previousState;
+
+    public int Gems = 0;
+    Vector3 checkpointPosition;
+
+    void Start()
+    {
+        checkpointPosition = transform.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        string tag = collision.gameObject.tag;
+
+        if(tag == "Gem")
+        {
+            Gems++;
+            Destroy(collision.gameObject);
+        }
+        else if(tag == "Checkpoint")
+        {
+            checkpointPosition = collision.gameObject.transform.position;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        string tag = collision.gameObject.tag;
+
+        if(tag == "Kill")
+        {
+            TeleportToCheckpoint();
+        }
+    }
+
+    public void TeleportToCheckpoint()
+    {
+        transform.position = checkpointPosition;
+    }
+}
